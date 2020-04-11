ed3f1128c73c45d45b05bed0e5c6f8e9db25963f
Alex Quevedo <alex@monkey.pe>
2020-04-11 06:09:36 -0500

    Fixes

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ionic-image-loader'),
+      dir: require('path').join(__dirname, '../../coverage/ionic-image-loader-v5'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
